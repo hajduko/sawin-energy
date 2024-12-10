7a86fd0701914771c557667d6ac7ce4ad7f65fbe
Hajdu Kolos <hajdu.kolos@edu.bme.hu>
2024-12-11 00:52:12 +0100

    Added basic navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router';
+import { Link, Route, Routes } from 'react-router';
 import './App.css';
 import Home from './pages/home/Home.page';
 import HeatPump from './pages/heatpump/HeatPump.page';
@@ -8,16 +8,23 @@ import Partners from './pages/partners/Partners.page';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/'>
-        <Route index element={<Home />} />
-        <Route path='heatpump' element={<HeatPump />} />
-        <Route path='eoft' element={<Eoft />} />
-        <Route path='commercial' element={<Commercial />} />
-        <Route path='partners' element={<Partners />} />
-        <Route path='*' element={<div>404 Not Found</div>} />
-      </Route>
-    </Routes>
+    <>
+      <Link to='/'>Home</Link>
+      <Link to='/heatpump'>Heat Pump</Link>
+      <Link to='/eoft'>EOFT</Link>
+      <Link to='/commercial'>Commercial</Link>
+      <Link to='/partners'>Partners</Link>
+      <Routes>
+        <Route path='/'>
+          <Route index element={<Home />} />
+          <Route path='heatpump' element={<HeatPump />} />
+          <Route path='eoft' element={<Eoft />} />
+          <Route path='commercial' element={<Commercial />} />
+          <Route path='partners' element={<Partners />} />
+          <Route path='*' element={<div>404 Not Found</div>} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
