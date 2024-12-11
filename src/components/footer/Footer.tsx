0c73b865e0c56f4bb6d251db2d3c50586fdc70b8
Hajdu Kolos <hajdu.kolos@edu.bme.hu>
2024-12-11 10:54:10 +0100

    Added footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,29 @@
+import { Box, Flex } from '@chakra-ui/react';
+import { Link } from 'react-router';
+
+const NavBar = () => {
+  return (
+    <Flex bgColor='#f00' justify='center'>
+      <Box bgColor='#0f0' w={{ base: '100%', md: '56rem', lg: '72rem' }}>
+        <Flex
+          minH={{ base: '3rem', md: '4rem' }}
+          w='100%'
+          align='center'
+          justify='space-between'
+          gap={6}
+          paddingX={{ base: '1rem' }}>
+          <Box marginEnd='auto'>
+            <Link to='/'>Logo</Link>
+          </Box>
+          <Link to='/'>Home</Link>
+          <Link to='/heatpump'>Heat Pump</Link>
+          <Link to='/eoft'>EOFT</Link>
+          <Link to='/commercial'>Commercial</Link>
+          <Link to='/partners'>Partners</Link>
+        </Flex>
+      </Box>
+    </Flex>
+  );
+};
+
+export default NavBar;
