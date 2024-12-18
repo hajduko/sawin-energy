aceed0aa39f64e2b5e2b0cc8418b969d5126dfc7
Hajdu Kolos <hajdu.kolos@edu.bme.hu>
2024-12-18 10:37:07 +0100

    Added Chakra UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { ChakraProvider } from '@chakra-ui/react';
+import { ColorModeProvider } from './components/ui/color-mode.tsx';
+import { system } from './util/configs/theme.config.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ChakraProvider value={system}>
+      <ColorModeProvider defaultTheme='light'>
+        <App />
+      </ColorModeProvider>
+    </ChakraProvider>
+  </StrictMode>
+);
---
+++ src/util/configs/theme.config.ts
@@ -0,0 +1,14 @@
+import { createSystem, defaultConfig } from '@chakra-ui/react';
+
+export const system = createSystem(defaultConfig, {
+  theme: {
+    semanticTokens: {
+      colors: {
+        primary: { value: '#007648' },
+        secondary: { value: '#efffe5' },
+        dark: { value: '#062617' },
+        accent: { value: '#0df157' },
+      },
+    },
+  },
+});
