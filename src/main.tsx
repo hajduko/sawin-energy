c944aacb6acebedd6328a3b08537ab6dfbbd25c4
Hajdu Kolos <hajdu.kolos@edu.bme.hu>
2024-12-18 10:38:57 +0100

    Added React Router

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,16 @@ import App from './App.tsx';
 import { ChakraProvider } from '@chakra-ui/react';
 import { ColorModeProvider } from './components/ui/color-mode.tsx';
 import { system } from './util/configs/theme.config.ts';
+import { BrowserRouter } from 'react-router';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ChakraProvider value={system}>
-      <ColorModeProvider defaultTheme='light'>
-        <App />
-      </ColorModeProvider>
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider value={system}>
+        <ColorModeProvider defaultTheme='light'>
+          <App />
+        </ColorModeProvider>
+      </ChakraProvider>
+    </BrowserRouter>
   </StrictMode>
 );
