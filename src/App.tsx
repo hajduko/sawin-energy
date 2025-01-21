43374a8103ac73457839cd5a047390458fc55b33
Hajdu Kolos <hajdu.kolos@edu.bme.hu>
2025-01-21 17:17:36 +0100

    Added scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,22 @@ import HeatPump from './pages/heatpump/HeatPump.page';
 import Privacy from './pages/privacy/Privacy.page';
 import Page404 from './pages/404/404.page';
 import Residential from './pages/applications/residential/Residential.page';
+import ScrollToTop from './components/scrolltotop/ScrollToTop';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/'>
-        <Route index element={<Home />} />
-        <Route path='heatpump' element={<HeatPump />} />
-        <Route path='applications/residential' element={<Residential />} />
-        <Route path='privacy' element={<Privacy />} />
-        <Route path='*' element={<Page404 />} />
-      </Route>
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path='/'>
+          <Route index element={<Home />} />
+          <Route path='heatpump' element={<HeatPump />} />
+          <Route path='applications/residential' element={<Residential />} />
+          <Route path='privacy' element={<Privacy />} />
+          <Route path='*' element={<Page404 />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
