4cb59b42cfc0894c5a2157ad18663e19bb3b9cd3
Kolos Hajdu <kolos@sawin.hu>
2025-10-27 11:12:16 +0100

    Changed faluprogram link

--- src/Redirect.tsx
+++ src/Redirect.tsx
@@ -1,5 +1,5 @@
 const Redirect = () => {
-  window.location.href = 'https://app.airrange.io/#/element/xr3b_6yjn9MeydSh4T7JmNTKBn';
+  window.location.href = 'https://app.airrange.io/#/element/xr3b_CC0b1szVF5kLO0z23uPkv';
   return <></>;
 };
 
